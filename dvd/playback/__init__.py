5117bf267089798df5acff58b2cf406666303213
Katherine <65572889+kthrnbeh@users.noreply.github.com>
2026-08-28 21:21:04 -0600

    Add ISweep host playback package

---
+++ dvd/playback/__init__.py
@@ -0,0 +1,20 @@
+"""ISweep DVD host-playback abstractions.
+
+The host-playback package lets ISweep own playback state without tying the
+Decision Engine to one media library or one television brand.
+"""
+
+from .controller import PlaybackController
+from .host import ISweepHostPlayback
+from .models import MediaKind, MediaSource, PlaybackState, PlaybackStatus
+from .simulated import SimulatedPlaybackController
+
+__all__ = [
+    "ISweepHostPlayback",
+    "MediaKind",
+    "MediaSource",
+    "PlaybackController",
+    "PlaybackState",
+    "PlaybackStatus",
+    "SimulatedPlaybackController",
+]
